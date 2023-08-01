c77bbb09ead7b3d54c2a5f8feb2855d035eb66db
Daniel Lemire <daniel@lemire.me>
2023-08-01 16:51:49 -0400

    Saving...

--- chapter5/linearregression.py
+++ chapter5/linearregression.py
@@ -1,28 +1,19 @@
 import matplotlib
 import pandas as pd 
 import matplotlib.pyplot as plt
-import numpy as np
-# Stochastic Gradient Descent
-#from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import LinearRegression
 
 modele = LinearRegression()
 df = pd.read_csv("globalmeantemp.csv")
-print(df.keys())
 x = df["Year"].values.reshape(-1,1)
 y = df["Nasa GISS"]
 
 modele.fit(y=y,X=x)
 print("intercept = ", modele.intercept_)
 print("slope = ", modele.coef_)
-print("warming per decade = ", modele.coef_[0]*10, "C/decade")
-print("warming per century = ", modele.coef_[0]*100, "C/century")
-
-print("temperature in 2000 = ", modele.predict([[2000]]), " C")
-print("temperature in 2050 = ", modele.predict([[2050]]), " C")
-print("temperature in 2100 = ", modele.predict([[2100]]), " C")
 r2 = modele.score(X=x,y=y)
-print(r2)
+
+print("temperature in 2100 = ", modele.predict([[2100]]), " C")
 
 
 X_modele = x
